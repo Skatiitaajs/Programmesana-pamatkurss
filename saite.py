da492f81abf655deaf48641e9255baac2b8aca8e
Skatiitaajs <raivis.bisters@gmail.com>
2026-09-16 12:44:50 +0300

    Update saite.py

--- saite.py
+++ saite.py
@@ -2,3 +2,4 @@ lietotajvards = "Skatiitaajs"
 kratuve = "programmesana-pamatkurss"
 saite = f"https://github.com/{lietotajvards}/{kratuve}"
 print(saite)
+#Burts f nozīmē aizstāt tekstā esosšās figūriekavas {}
